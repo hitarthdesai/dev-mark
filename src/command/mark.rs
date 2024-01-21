a0d70edfdfb602969e62af109b824b51e92f5426
Hitarth Desai <hitarth.desai@adevinta.com>
2024-01-20 23:50:24 -0500

    args and inputs clean-up

--- src/command/mark.rs
+++ src/command/mark.rs
@@ -1,6 +1,6 @@
 use std::ffi::OsStr;
 use chrono::{NaiveDate};
-use inquire::{DateSelect, Editor, required, Text};
+use inquire::{Editor, required, Text};
 use tokio_postgres::Client;
 use crate::config;
 
@@ -8,16 +8,15 @@ use crate::config;
 struct InputMark {
     title: Option<String>,
     note: String,
-    date: Option<NaiveDate>,
 }
 
-fn get_input_for_mark(should_provide_date_picker: bool) -> InputMark {
+fn get_input_for_mark() -> InputMark {
     let guard = config::CONFIG.lock().unwrap();
     let config = guard.as_ref().unwrap();
     let mark_style = &config.mark_style;
     let editor = &config.editor;
 
-    let mut input_mark = match mark_style {
+    let input_mark = match mark_style {
         config::MarkStyle::Default => {
             let _note = Text::new("Mark")
                 .with_placeholder("Some text to be marked")
@@ -28,7 +27,6 @@ fn get_input_for_mark(should_provide_date_picker: bool) -> InputMark {
             InputMark {
                 title: None,
                 note: _note,
-                date: None,
             }
         },
 
@@ -48,35 +46,20 @@ fn get_input_for_mark(should_provide_date_picker: bool) -> InputMark {
             InputMark {
                 title: Some(_title),
                 note: _note,
-                date: None,
             }
         },
     };
 
-    match should_provide_date_picker {
-        true => {
-            let _date = DateSelect::new("Date")
-                .with_help_message("Enter date.rs for mark")
-                .prompt().unwrap();
-
-            input_mark.date = Some(_date);
-        },
-        false => {},
-    }
-
     return input_mark;
 }
 
-pub async fn add_mark(client: &Client, should_provide_date_picker: bool) -> std::io::Result<()> {
-    let input = get_input_for_mark(should_provide_date_picker);
+pub async fn add_mark(client: &Client, date: NaiveDate) -> std::io::Result<()> {
+    let input = get_input_for_mark();
 
     let statement = client.prepare("INSERT INTO marks (title, note, created_at) VALUES ($1, $2, $3) RETURNING id").await.expect("Could not prepare statement");
 
     let _title = &input.title.unwrap_or(String::new());
-    let _created_at = &match input.date {
-        Some(d) => { d.and_hms_opt(0, 0, 0).unwrap() },
-        None => { chrono::Local::now().naive_local() }
-    };
+    let _created_at = &date.and_hms_opt(0, 0, 0);
 
     client.query(&statement, &[_title, &input.note, _created_at]).await.expect("Could not execute query");
 
