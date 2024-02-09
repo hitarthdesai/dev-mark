2e7a61f96f612e2dc17e8b2cabfd0fc4a8c875a4
Hitarth Desai <hitarth.desai@adevinta.com>
2024-02-08 22:06:15 -0500

    streamline date argument

--- src/argument/date.rs
+++ src/argument/date.rs
@@ -2,61 +2,50 @@ use chrono::{Duration, NaiveDate};
 use inquire::DateSelect;
 
 enum DateOption {
-    Past,
+    Custom,
     Exact(NaiveDate),
-    Future
 }
 
 pub fn get_date_from_args(_arg: Option<String>) -> Result<NaiveDate, &'static str> {
-    let arg = match _arg {
-        None => { return Err("No date option provided. Expected one of --past, --today, --future"); },
-        Some(a) => { a }
-    };
+    let date_option = match _arg {
+        None => { DateOption::Custom },
+        Some(a) => {
+            match a.starts_with("--today") {
+                true => {
+                    let mut anchor = chrono::Local::now().date_naive();
+                    let offset = a.replace("--today", "");
 
-    let date_option = match arg.as_str() {
-        "--past" => DateOption::Past,
-        "--future" => DateOption::Future,
-        _ => {
-            if arg.starts_with("--today") {
-                let mut anchor = chrono::Local::now().date_naive();
-                let offset = arg.replace("--today", "");
+                    anchor += match offset.chars().next() {
+                        None => {
+                            Duration::days(0)
+                        },
+                        Some('+') => {
+                            let offset = offset[1..].parse::<i64>().expect("Invalid offset");
+                            Duration::days(offset)
+                        },
+                        Some('-') => {
+                            let offset = offset[1..].parse::<i64>().expect("Invalid offset");
+                            Duration::days(-offset)
+                        },
+                        Some(_) => {
+                            return Err("Invalid operator");
+                        }
+                    };
 
-                anchor += match offset.chars().next() {
-                    None => {
-                        Duration::days(0)
-                    },
-                    Some('+') => {
-                        let offset = offset[1..].parse::<i64>().expect("Invalid offset");
-                        Duration::days(offset)
-                    },
-                    Some('-') => {
-                        let offset = offset[1..].parse::<i64>().expect("Invalid offset");
-                        Duration::days(-offset)
-                    },
-                    Some(_) => {
-                        return Err("Invalid operator");
-                    }
-                };
-
-                DateOption::Exact(anchor)
-            } else {
-                return Err("Invalid date option")
+                    DateOption::Exact(anchor)
+                },
+                false => {
+                    DateOption::Custom
+                }
             }
         }
     };
 
     let date = match date_option {
-        DateOption::Past => {
-            DateSelect::new("Date")
-                .with_max_date(chrono::Local::now().date_naive() - Duration::days(1))
-                .with_help_message("Enter a past date for mark")
-                .prompt().unwrap()
-        },
         DateOption::Exact(date) => { date },
-        DateOption::Future => {
+        DateOption::Custom => {
             DateSelect::new("Date")
-                .with_min_date(chrono::Local::now().date_naive() + Duration::days(1))
-                .with_help_message("Enter a past date for mark")
+                .with_help_message("Enter a date for mark")
                 .prompt().unwrap()
         }
     };
