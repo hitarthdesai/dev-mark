3e36c1ec13df5d98d27ffb712bd1b9bee2549462
Hitarth Desai <hitarthdesai306@gmail.com>
2024-04-12 21:09:38 -0400

    cleanup config and setup

--- src/db/connect.rs
+++ src/db/connect.rs
@@ -1,14 +1,15 @@
-// use crate::config::CONFIG;
+use crate::config::CONFIG;
 use sqlx::{SqlitePool, error::Error, Pool, Sqlite};
 
 /**
  * Connect to the database
  */
 pub async fn connect_to_db() -> Result<Pool<Sqlite>, Error> {
-    // let guard = CONFIG.lock().unwrap();
-    // let config = guard.as_ref().unwrap();
-    // let connect_string = config.connect_string.as_str();
+    let guard = CONFIG.lock().unwrap();
+    let config = guard.as_ref().unwrap();
+    let database_url = config.database_url.as_str();
 
-    let pool = SqlitePool::connect("sqlite:database.db").await?;
+
+    let pool = SqlitePool::connect(database_url).await?;
     Ok(pool)
 }
